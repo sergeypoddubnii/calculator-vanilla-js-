540dc23c14b71acbdd514b95bece8a3a6c6090c4
sergey <sergey_pidd@ukr.net>
2020-12-14 23:01:42 +0200

    add redux

--- src/pages/Calculator/index.tsx
+++ src/pages/Calculator/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Number from "./components/Number";
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const Calculator = () => {
-  return <h1>calculator</h1>;
+  const numberBtns = numbers.map((num: number) => {
+    return <Number key={num} value={num} />;
+  });
+  return (
+    <>
+      <h1>calculator</h1>
+      {numberBtns}
+    </>
+  );
 };
 
 export default Calculator;
