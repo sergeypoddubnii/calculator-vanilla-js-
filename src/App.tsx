90a86080c117f54053508976d71f21e454118cd4
sergey <sergey_pidd@ukr.net>
2020-12-14 17:41:25 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import routes from "./routes";
+import Calculator from "./pages/Calculator";
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route path={routes.CALCULATOR} component={Calculator} />
+          <Redirect to={routes.CALCULATOR} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+const routes = {
+  CALCULATOR: "/calculator",
+};
+
+export default routes;
