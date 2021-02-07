4597e3086ca9e92ba7b565d36c5f8951a04de5a8
Temporary Username <temp@Temporarys-MacBook-Pro.local>
2021-02-07 12:40:08 +0200

    fix api

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const baseUrl = "https://api.weatherbit.io/v2.0/";
 const keyApi = "&key=0812a8100941482089950b497b8cb0f7";
-const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+// const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
 export const loadCurrentWeatherByCity = (city): any =>
-  axios.get(`${proxyUrl}${baseUrl}/current?city=${city}${keyApi}`);
+  axios.get(`${baseUrl}/current?city=${city}${keyApi}`);
 
 export const loadCurrentWeatherByGeo = (lat: any, lon: any): any =>
-  axios.get(`${proxyUrl}${baseUrl}/current&lat=${lat}&lon=${lon}${keyApi}`);
+  axios.get(`${baseUrl}/current&lat=${lat}&lon=${lon}${keyApi}`);
