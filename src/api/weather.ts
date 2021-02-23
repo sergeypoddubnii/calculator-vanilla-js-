da235bb98e228a58858e77420ee814450eceab4c
Temporary Username <temp@Temporarys-MacBook-Pro.local>
2021-02-23 10:20:18 +0200

    add new api key

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseUrl = "https://api.weatherbit.io/v2.0/";
-const keyApi = "&key=0812a8100941482089950b497b8cb0f7";
+const keyApi = "&key=c74bebc4abf546899b3d706e3a7d7bb2";
 // const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
 export const loadCurrentWeatherByCity = (city): any =>
@@ -12,9 +12,10 @@ export const loadCurrentWeatherByGeo = (lat: any, lon: any): any =>
 
 // load hourly forecast 24 hours
 export const loadForecastHourlyByCity = (city): any =>
-  axios.get(`${baseUrl}/forecast/hourly?city=${city}${keyApi}&hours=24`);
+  axios.get(`${baseUrl}/forecast/daily?city=${city}${keyApi}`);
 
 export const loadForecasthourlyByGeo = (lat: any, lon: any): any =>
   axios.get(
     `${baseUrl}/forecast/hourly?lat=${lat}&lon=${lon}${keyApi}&hours=24`
   );
+// https://api.weatherbit.io/v2.0/forecast/hourly?city=Raleigh,NC&key=API_KEY&hours=48
--- src/pages/Weather/index.tsx
+++ src/pages/Weather/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { getInfoGeolocation } from "../../redux/weather/actions";
 import Form from "./components/Form";
 import GeneralDescription from "./components/GerenalDescription/index";
@@ -8,12 +8,14 @@ const Weather = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(getPosition);
+      console.log(navigator.geolocation.getCurrentPosition(getPosition));
     }
     function getPosition({ coords }) {
+      console.log(coords);
+      console.log("location");
       dispatch(getInfoGeolocation(coords.latitude, coords.longitude));
     }
-  }, [dispatch]);
+  }, []);
 
   return (
     <>
